54a20d9faa014d0270d6cdb2ce746756ae6ea9ec
JovanaRadisavljevic <radisavljevic435@gamil.com>
2024-07-14 20:39:01 +0200

    prvi commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css"
+//pokrecem aplikaciju: npm start
+
+function App() {
+  return (
+    <div className="App">
+      <div className="main">main</div>
+      <div className="pyramid">money</div>
+    </div>
+  );
+}
+
+export default App;
